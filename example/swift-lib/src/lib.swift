8b8aede947e7e654d781d1897235344bc2e1f04d
Brendan Allan <brendonovich@outlook.com>
2022-03-24 03:13:59 +0800

    Fix getMounts memory leaks

--- example/swift-lib/src/lib.swift
+++ example/swift-lib/src/lib.swift
@@ -42,30 +42,34 @@ public func getMounts() -> SRObjectArray {
         .volumeIsRootFileSystemKey,
     ]
     
-    let paths = FileManager().mountedVolumeURLs(includingResourceValuesForKeys: keys, options: [])
+    var paths = autoreleasepool {
+        FileManager().mountedVolumeURLs(includingResourceValuesForKeys: keys, options: [])
+    }
     
     var validMounts: [Volume] = []
         
     if let urls = paths {
-        for url in urls {
-            let components = url.pathComponents
-            if components.count == 1 || components.count > 1
-               && components[1] == "Volumes"
-            {
-                let metadata = try? url.promisedItemResourceValues(forKeys: Set(keys))
-                
-                let volume = Volume(
-                    name: metadata?.volumeName ?? "",
-                    path: url.path,
-                    total_capacity: metadata?.volumeTotalCapacity ?? 0,
-                    available_capacity: metadata?.volumeAvailableCapacity ?? 0,
-                    is_removable: metadata?.volumeIsRemovable ?? false,
-                    is_ejectable: metadata?.volumeIsEjectable ?? false,
-                    is_root_filesystem: metadata?.volumeIsRootFileSystem ?? false
-                )
-                
-                
-                validMounts.append(volume)
+        autoreleasepool {
+            for url in urls {
+                let components = url.pathComponents
+                if components.count == 1 || components.count > 1
+                   && components[1] == "Volumes"
+                {
+                    let metadata = try? url.promisedItemResourceValues(forKeys: Set(keys))
+                    
+                    let volume = Volume(
+                        name: metadata?.volumeName ?? "",
+                        path: url.path,
+                        total_capacity: metadata?.volumeTotalCapacity ?? 0,
+                        available_capacity: metadata?.volumeAvailableCapacity ?? 0,
+                        is_removable: metadata?.volumeIsRemovable ?? false,
+                        is_ejectable: metadata?.volumeIsEjectable ?? false,
+                        is_root_filesystem: metadata?.volumeIsRootFileSystem ?? false
+                    )
+                    
+                    
+                    validMounts.append(volume)
+                }
             }
         }
     }

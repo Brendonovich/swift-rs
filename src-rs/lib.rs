fe5a1c2f668e6bade43d6f56e2530f110055cee9
Brendan Allan <brendonovich@outlook.com>
2022-06-14 20:07:19 +0800

    pub_swift_fn

--- src-rs/lib.rs
+++ src-rs/lib.rs
@@ -13,44 +13,66 @@ pub use swift_ret::SwiftRet;
 pub mod build;
 
 #[macro_export]
+macro_rules! swift_fn_inner {
+    ($name: ident($($arg: ident: $t: ty),*)) => {{
+        extern "C" {
+            fn $name($($arg: <$t as $crate::SwiftArg>::SwiftType),*);
+        }
+
+        $(let $arg = $crate::SwiftArg::to_swift_rs_type($arg);)*
+
+        unsafe {
+            $name($($arg),*);
+        }
+    }};
+    ($name: ident($($arg: ident: $t: ty),*) -> $ret: ty) => {{
+        extern "C" {
+            fn $name($($arg: <$t as $crate::SwiftArg>::SwiftType),*) -> <$ret as $crate::SwiftRet>::SwiftType;
+            fn objc_autoreleasePoolPush() -> *mut std::os::raw::c_void;
+            fn objc_autoreleasePoolPop(context: *mut std::os::raw::c_void);
+        }
+
+        let pool = unsafe { objc_autoreleasePoolPush() };
+
+        $(
+            let $arg = <$t as $crate::SwiftArg>::to_swift_rs_type($arg);
+            let $arg = <$t as $crate::SwiftArg>::as_swift_type(&$arg);
+        )*
+
+
+        unsafe {
+            let ret = $name($($arg),*);
+            <$ret as $crate::SwiftRet>::__retain(&ret);
+            objc_autoreleasePoolPop(pool);
+            ret
+        }
+    }};
+}
+
+#[macro_export]
 macro_rules! swift_fn {
     ($name: ident($($arg: ident: $t: ty),*)) => {
         fn $name($($arg: $t),*) {
-            extern "C" {
-                fn $name($($arg: <$t as $crate::SwiftArg>::SwiftType),*);
-            }
-
-            $(let $arg = $crate::SwiftArg::to_swift_rs_type($arg);)*
-
-            unsafe {
-                $name($($arg),*);
-            }
+            $crate::swift_fn_inner!($name($($arg: $t),*))
         }
     };
     ($name: ident($($arg: ident: $t: ty),*) -> $ret: ty) => {
         fn $name($($arg: $t),*) -> <$ret as $crate::SwiftRet>::SwiftType {
-            extern "C" {
-                fn $name($($arg: <$t as $crate::SwiftArg>::SwiftType),*) -> <$ret as $crate::SwiftRet>::SwiftType;
-            }
-            extern "C" {
-                fn objc_autoreleasePoolPush() -> *mut std::os::raw::c_void;
-                fn objc_autoreleasePoolPop(context: *mut std::os::raw::c_void);
-            }
+            $crate::swift_fn_inner!($name($($arg: $t),*) -> $ret)
+        }
+    };
+}
 
-            let pool = unsafe { objc_autoreleasePoolPush() };
-
-            $(
-                let $arg = <$t as $crate::SwiftArg>::to_swift_rs_type($arg);
-                let $arg = <$t as $crate::SwiftArg>::as_swift_type(&$arg);
-            )*
-            
-
-            unsafe {
-                let ret = $name($($arg),*);
-                <$ret as $crate::SwiftRet>::__retain(&ret);
-                objc_autoreleasePoolPop(pool);
-                ret
-            }
+#[macro_export]
+macro_rules! pub_swift_fn {
+    ($name: ident($($arg: ident: $t: ty),*)) => {
+        pub fn $name($($arg: $t),*) {
+            $crate::swift_fn_inner!($name($($arg: $t),*))
+        }
+    };
+    ($name: ident($($arg: ident: $t: ty),*) -> $ret: ty) => {
+        pub fn $name($($arg: $t),*) -> <$ret as $crate::SwiftRet>::SwiftType {
+            $crate::swift_fn_inner!($name($($arg: $t),*) -> $ret)
         }
     };
 }

7db579c09a8bdc6b2c953c4599c21e4c54e4499a
Amod Malviya <amod@rootnet.in>
2023-01-10 09:03:46 +0530

    tests: include test for reflection, and make all tests be checked for leaks

--- tests/swift-pkg/lib.swift
+++ tests/swift-pkg/lib.swift
@@ -4,3 +4,8 @@ import SwiftRs
 func getGreeting(name: UnsafePointer<SRString>) -> SRString {
     return toRust(SRString("Hello \(name.pointee.to_string())"))
 }
+
+@_cdecl("reflect_string")
+func reflectString(ptr: UnsafePointer<SRString>) -> SRString {
+    return ptr.pointee
+}

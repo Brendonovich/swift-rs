970f7c70322fbf101e4e019296628ca945bf7bee
Ali AlSalman <alireza.salman@icloud.com>
2024-07-19 06:23:20 +0300

    deprecate `SRObjectArray`, make `SRArray` and `SRString` final

--- src-swift/lib.swift
+++ src-swift/lib.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public class SRArray<T>: NSObject, ExpressibleByArrayLiteral {
+public final class SRArray<T>: NSObject, ExpressibleByArrayLiteral {
     public typealias ArrayLiteralElement = T
     
     // Used by Rust
@@ -9,7 +9,7 @@ public class SRArray<T>: NSObject, ExpressibleByArrayLiteral {
     
     // Actual array, deallocates objects inside automatically
     let array: [T]
-
+    
     public override init() {
         self.array = []
         self.pointer = UnsafePointer(self.array)
@@ -22,7 +22,7 @@ public class SRArray<T>: NSObject, ExpressibleByArrayLiteral {
         self.length = data.count
     }
     
-    public required init(arrayLiteral elements: T...) {
+    public init(arrayLiteral elements: T...) {
         self.array = elements
         self.pointer = UnsafePointer(elements)
         self.length = elements.count
@@ -33,13 +33,8 @@ public class SRArray<T>: NSObject, ExpressibleByArrayLiteral {
     }
 }
 
-public class SRObjectArray: NSObject {
-    let data: SRArray<NSObject>
-    
-    public init(_ data: [NSObject]) {
-        self.data = SRArray(data)
-    }
-}
+@available(*, deprecated, message: "use SRArray<NSObject> instead")
+typealias SRObjectArray = SRArray<NSObject>
 
 public class SRData: NSObject {
     let data: SRArray<UInt8>
@@ -61,7 +56,7 @@ public class SRData: NSObject {
     }
 }
 
-public class SRString: SRData, ExpressibleByStringLiteral {
+public final class SRString: SRData, ExpressibleByStringLiteral {
     public typealias StringLiteralType = String
     
     public override init() {
@@ -72,11 +67,11 @@ public class SRString: SRData, ExpressibleByStringLiteral {
         super.init(Array(string.utf8))
     }
 
-    public required init(stringLiteral value: String) {
+    public init(stringLiteral value: String) {
         super.init(Array(value.utf8))
     }
     
-    init(_ data: SRData) {
+    public init(_ data: SRData) {
         super.init(data.data)
     }
 
